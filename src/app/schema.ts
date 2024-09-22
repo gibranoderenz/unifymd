8c4b81b8ea364db49eba3cb8b4216b333db09fe9
Gibrano Derenz <gibranoderenz123@gmail.com>
2024-09-22 05:07:50 -0400

    fix: add patient form schema handling

--- src/app/schema.ts
+++ src/app/schema.ts
@@ -1,4 +1,41 @@
+// Source for the phone number fields:
+// https://github.com/colinhacks/zod/issues/3378#issuecomment-2067591844
+
 import { patients } from "@/app/drizzle/schema";
 import { createInsertSchema } from "drizzle-zod";
+import parsePhoneNumber from "libphonenumber-js";
+import { z } from "zod";
 
-export const createPatientSchema = createInsertSchema(patients);
+export const createPatientSchema = createInsertSchema(patients).extend({
+  email: z.string().email(),
+  phoneNumber: z.string().transform((value, ctx) => {
+    const phoneNumber = parsePhoneNumber(value, {
+      defaultCountry: "US",
+    });
+
+    if (!phoneNumber?.isValid()) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Invalid phone number",
+      });
+      return z.NEVER;
+    }
+
+    return phoneNumber.formatInternational();
+  }),
+  emergencyContact: z.string().transform((value, ctx) => {
+    const phoneNumber = parsePhoneNumber(value, {
+      defaultCountry: "US",
+    });
+
+    if (!phoneNumber?.isValid()) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Invalid phone number",
+      });
+      return z.NEVER;
+    }
+
+    return phoneNumber.formatInternational();
+  }),
+});
